e50bc39880801f1a1f96baeee0575d7b30a25b80
rabbitwhile <vallsarnau19@gmail.com>
2015-11-13 13:21:41 +0100

    Update client.hh

--- client.hh
+++ client.hh
@@ -8,10 +8,23 @@ class client{
   private:
     int torn //numero de torn
     int caixa //numero de caixa
-    int productes //nombre de unitats de productes
+    int productes //nombre de unitats de productes. 
+    //Sactualitza cada cop que s'afegeix un producte
     Rellotge R //HH:MM:SS del tiquet
+    
   public:
   
+  /* Constructora */
+  /** @brief Creadora per defecte.
+   * \pre Cert
+   * \post El resultat es un producte sense inicialitzar
+  */
+  
+  /*Destructora */
+  /** @brief Destructora per defecte
+   * \pre Existeix un producte
+   * \post Destrueix el producte
+  */ 
   
   
   
