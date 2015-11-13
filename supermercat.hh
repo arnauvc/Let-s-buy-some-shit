9df3078a0dff0c1b1eb3efbd07738cfef26c3760
rabbitwhile <vallsarnau19@gmail.com>
2015-11-13 22:31:59 +0100

    Update supermercat.hh

--- supermercat.hh
+++ supermercat.hh
@@ -9,6 +9,7 @@ class supermercat{
     int rengles // nombre de rengles
     int columnes // nombre de columnes
     int caixes // nombre de caixes
+    vector < pair<productes,int>> // sort i despres stable_sort
     map<> //Conte tots els productes possibles, mapejats segons seccio i ordenats alfabeticament
   
   
