9c4d6d42210520ed120f6b0305f5c97432907ced
rabbitwhile <vallsarnau19@gmail.com>
2015-11-13 13:19:59 +0100

    Update client.hh

--- client.hh
+++ client.hh
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "Rellotge.hh"
 
+using namespace std;
+
+class client{
+   
+  private:
+    int torn //numero de torn
+    int caixa //numero de caixa
+    int productes //nombre de unitats de productes
+    Rellotge R //HH:MM:SS del tiquet
+  public:
+  
+  
+  
+  
+  
+}
