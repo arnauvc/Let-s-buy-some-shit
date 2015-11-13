65d9d38ac6209ddbc3aa8e35b2443e2242d86a29
rabbitwhile <vallsarnau19@gmail.com>
2015-11-13 13:26:38 +0100

    Update supermercat.hh

--- supermercat.hh
+++ supermercat.hh
@@ -2,3 +2,15 @@
 #include <strng>
 
 using namespace std;
+
+class supermercat{
+  
+  public:
+    int rengles // nombre de rengles
+    int columnes // nombre de columnes
+    int caixes // nombre de caixes
+    map<> //Conte tots els productes possibles, mapejats segons seccio i ordenats alfabeticament
+  
+  
+  private:
+}
