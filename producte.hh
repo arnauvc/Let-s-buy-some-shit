08384cc410c4cd29d4611485257aba11be8a123f
luisxu <luisxu95@gmail.com>
2015-11-17 15:00:06 +0100

    Update producte.hh

--- producte.hh
+++ producte.hh
@@ -34,12 +34,20 @@ class Producte{
   /*Consultores*/
   
   string consulta_nom(string nom);
+  /* Pre: cert */
+  /* Post: el resultat es el nom del parametre implicit */
   
   string consulta_seccio(string seccio);
+  /* Pre: cert */
+  /* Post: el resultat es la seccio del parametre implicit */
   
   double consulta_preu(double preu);
+  /* Pre: el parametre implicit te preu */
+  /* Post: el resultat es el preu del parametre implicit */
   
   int consulta_temps(int temps_cobrament);
+  /* Pre: cert */
+  /* Pro: el resultat es el temps de cobrament del parametre implicit */
   /** @brief Consulta
    * 
   */
@@ -49,7 +57,16 @@ class Producte{
    * \post Escriu per pantalla el nom, la seccio,
    * el preu i el temps de cobrament del producte del parametre implicit
   */
-  void llegir();
+  void llegir_producte();
+  /* Pre: hi ha preparats al canal estandar d'entrada */
+  /* Post: el parametre implicit passa a tenir els atributs llegits del canal estandard d'entrada */
   
   void escriu_producte() const;
-}
+  /* Pre: cert */
+  /* Post: s'han escrit els atributs del parametre implicit al canal estandard de sortida */
+  
+  
+
+};
+#endif
+
