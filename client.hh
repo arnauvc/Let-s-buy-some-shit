86a749a3dad9f63fc0f8c1e1ac4d5e9d0662196e
rabbitwhile <vallsarnau19@gmail.com>
2015-11-17 13:18:20 +0100

    Update client.hh

--- client.hh
+++ client.hh
@@ -17,48 +17,60 @@ class Client{
   
   /* Constructora */
   
-  Client();
-  
-  Client(int torn, int caixa, int producte, Rellotge R);
   /** @brief Creadora per defecte.
    * \pre Cert
    * \post El resultat es un Client amb el torn, la caixa, numero
    * de productes i lhora de la compra
   */
+  Client();
+  
+  
+  /** @brief Creadora amb parametres.
+   * \pre Cert
+   * \post El resultat es un Client amb el torn, la caixa, numero
+   * de productes i lhora de la compra
+  */
+  Client(int torn, int caixa, int producte, Rellotge R);
   
   /*Destructora */
   
-  ~Client();
   /** @brief Destructora per defecte
    * \pre Existeix un Client
    * \post Destrueix el Client
-  */ 
+  */
+  ~Client();
 
    /*Modificadors*/
    
-   void afegir_caixa(int caixa);
+   
    /* Pre: el parametre implicit no te #caixa */
    /* Post: el resultat es el #caixa */
+   void afegir_caixa(int caixa);
    
-   void modificar_caixa(int caixa);
+   
    /* Pre: el parametre implicit ja te caixa pero es massa lent */
    /* Post: el resultat es un canvi de caixa */
+   void modificar_caixa(int caixa);
    
-   void afegir_producte(int producte);
+   
    /* Pre: el parametre implicit no te productes */
    /* Post: afegeix productes al parametre implicit */
+   void afegir_producte(int producte);
    
    /*Consultors*/
    
-   int consultar_torn(int torn) const;
+   
    /* Pre: cert */
    /* Pro: el resultat es el torn del client del parametre implicit*/
+   int consultar_torn(int torn) const;
    
-   int consultar_caixa(int caixa);
+   
    /* Pre: cert */
    /* Pro: el resultat la caixa que pertoca al client del parametre implicit*/
+   int consultar_caixa(int caixa);
    
-   Rellotge consulta_hora(Rellotge R) const;
+  
    /* Pre: cert */
    /* Pro: el resultat es la hora del ticker del parametre implicit*/
+    Rellotge consulta_hora(Rellotge R) const;
 }
