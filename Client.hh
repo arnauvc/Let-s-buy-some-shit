03be5ef3b300ee6fc569f8e8da0d062ba0d247bd
luisxu <luisxu95@gmail.com>
2015-11-17 16:10:41 +0100

    Update Client.hh

--- Client.hh
+++ Client.hh
@@ -18,16 +18,16 @@ class Client{
   /* Constructora */
   
   /** @brief Creadora per defecte.
-   * \pre Cert
-   * \post El resultat es un Client amb el torn, la caixa, numero
+   * \Pre: Cert
+   * \Post: El resultat es un Client amb el torn, la caixa, numero
    * de productes i lhora de la compra
   */
   Client();
   
   
   /** @brief Creadora amb parametres.
-   * \pre Cert
-   * \post El resultat es un Client amb el torn, la caixa, numero
+   * \Pre: Cert
+   * \Post: El resultat es un Client amb el torn, la caixa, numero
    * de productes i lhora de la compra
   */
   Client(int torn, int caixa, int producte, Rellotge R);
@@ -35,48 +35,48 @@ class Client{
   /*Destructora */
   
   /** @brief Destructora per defecte
-   * \pre Existeix un Client
-   * \post Destrueix el Client
+   * \Pre: Existeix un Client
+   * \Post: Destrueix el Client
   */
   ~Client();
 
    /*Modificadors*/
    
    /** @brief Afegeix numero de caixa
-    * \pre: el parametre implicit no te #caixa 
-    * \post: el resultat es el #caixa 
+    * \Pre: el parametre implicit no te #caixa 
+    * \Post: el resultat es el #caixa 
    */
    void afegir_caixa(int caixa);
    
    /** @brief
-    * \pre: el parametre implicit ja te caixa pero es massa lent 
-    * \post: el resultat es un canvi de caixa 
+    * \Pre: el parametre implicit ja te caixa pero es massa lent 
+    * \Post: el resultat es un canvi de caixa 
    */
    void modificar_caixa(int caixa);
    
    /** @brief
-    * \pre: el parametre implicit no te productes 
-    * \post: afegeix productes al parametre implicit
+    * \Pre: el parametre implicit no te productes 
+    * \Post: afegeix productes al parametre implicit
    */
    void afegir_producte(int producte);
    
    /*Consultores*/
    
    /** @brief Consulta el torn del client
-    * \pre: cert
-    * \post: el resultat es el torn del client del parametre implicit
+    * \Pre: cert
+    * \Post: el resultat es el torn del client del parametre implicit
    */
    int consultar_torn(int torn) const;
    
    /** @brief
-    * \pre: cert
-    * \post: el resultat la caixa que pertoca al client del parametre implicit
+    * \Pre: cert
+    * \Post: el resultat la caixa que pertoca al client del parametre implicit
    */
    int consultar_caixa(int caixa);
    
    /** @brief
-    * \pre: cert
-    * \post: el resultat es la hora del ticker del parametre implicit
+    * \Pre: cert
+    * \Post: el resultat es la hora del ticker del parametre implicit
    */
     
    Rellotge consulta_hora(Rellotge R) const;
