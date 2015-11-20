0901cf137ad2725533cf95d0c72aa6e33c047057
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 12:15:13 +0100

    Update Client.cc

--- Client.cc
+++ Client.cc
@@ -1,1 +1,24 @@
+  Client::Client();
+  
+  
+  
+  Client::Client(int torn, int caixa, int producte, Rellotge R);
+  
+  
+  Client::~Client();
 
+   
+   void Client::afegir_torn(int torn);
+  
+
+   void Client::afegir_caixa(int caixa);
+  
+   void Client::modificar_caixa(int caixa);
+   
+   void Client::afegir_producte(int producte);
+   
+   int Client::consultar_torn(int torn) const;
+   
+   int Client::consultar_caixa(int caixa);
+   
+   Rellotge Client::consulta_hora(Rellotge R) const;
