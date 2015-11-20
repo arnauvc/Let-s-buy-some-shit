70df18cfec489c0b67bbb0e0d77c3fb2d951932c
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 12:27:57 +0100

    Update Rellotge.cc

--- Rellotge.cc
+++ Rellotge.cc
@@ -13,36 +13,6 @@ string Rellotge::consulta_hora() {
 	return hora;
 }
 
-string Rellotge::consulta_data() {
-	return data;
-}
-
-int Rellotge::compara_dates(string data1, string data2) {
-	if (data1==data2) return 0;
-	else {
-		int a1, a2;
-		a1=(data1[6]*10)+data1[7];
-		a2=(data2[6]*10)+data2[7];
-		if (a1>a2) return 2;
-		if (a2>a1) return 1;
-		else {
-			int m1, m2;
-			m1=(data1[3]*10)+data1[4];
-			m2=(data2[3]*10)+data2[4];
-			if (m1>m2) return 2;
-			if (m2>m1) return 1;
-			else {
-				int d1, d2;
-				d1=(data1[0]*10)+data1[1];
-				d2=(data2[0]*10)+data2[1];
-				if (d1>d2) return 2;
-				else return 1;
-			}
-		}
-	}
-}
-
-
 
 int Rellotge::compara_hores(string hora1, string hora2) {
 	if (hora1==hora2) return 0;
