9ac5d5c5b357f7127c04c36e5d80214d29c2a814
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 12:12:47 +0100

    Update Supermercat.cc

--- Supermercat.cc
+++ Supermercat.cc
@@ -10,9 +10,7 @@ void Supermercat::inicialitza(r,c,x,n){
   caixes = x;
   Producte p;
   for (int i=0; i<n; ++i){
-    p.Producte();//vull que la creadora s'encarregui dels parametres
-    vec_productes[i].first = p.consulta_seccio();
-    vec_productes[i].second = p;
+    vec_productes[i][j].insert(p.vProducte()); //vProducte omple tots els camps del producte
 }
 
 void Supermercat::carrega(){
