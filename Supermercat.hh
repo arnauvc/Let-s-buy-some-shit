b4a09ffc6b2cef95b604d1fc6dd086b8d47a2520
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 12:34:05 +0100

    Update Supermercat.hh

--- Supermercat.hh
+++ Supermercat.hh
@@ -63,7 +63,7 @@ class Supermercat{
    * \pre Supermercat inicialitzat i carregat
    * \post Escriu per pantalla tots els productes que pertanyen a la seccio especificada
   */
-  void producte_seccio(); //escriu per pantalla tots els productes de la seccio
+  void producte_seccio(s); //escriu per pantalla tots els productes de la seccio
   // o bé "seccio buida" si no hi ha productes a la seccio
   
   /** @brief Busca el millor cami per recorre el supermercat
