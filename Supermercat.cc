4fdfb906e861ad418f70d473161aa2e97b614ea2
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 10:17:27 +0100

    Update Supermercat.cc

--- Supermercat.cc
+++ Supermercat.cc
@@ -1,1 +1,30 @@
+#include "Supermercat.hh"
 
+Supermercat::Supermercat() {}
+
+Supermercat::~Supermercat() {}
+
+void Supermercat::inicialitza(){
+  
+}
+
+void Supermercat::carrega(){
+  
+}
+
+void Supermercat::simula(){
+  
+}
+
+void informa(){
+  
+}
+
+void producte_seccio(){
+  
+}
+
+void millor_cami(){
+  
+}
+  
