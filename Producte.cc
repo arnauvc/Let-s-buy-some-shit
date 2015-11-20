e467f3320096e622b43a19efff1adafc66ad64b9
luisxu <luisxu95@gmail.com>
2015-11-20 12:13:10 +0100

    Update Producte.cc

--- Producte.cc
+++ Producte.cc
@@ -2,6 +2,26 @@
 
 // CREADORES //
 
+Producte::~Producte() {}
 
+void Producte::vProducte() {
+  cin >> nom >> seccio >> preu >> temps_cobrament;
+}
+
+string Producte::consulta_nom() {
+  return nom;
+}
+
+double Producte::consulta_preu() {
+  return preu;
+}
+
+string Producte::consulta_seccio() {
+  return seccio;
+}
+
+
+
+void llegir_producte();
 
 
