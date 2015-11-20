5e597b8dc6dd0aeb0d8d778da326448a624c3dad
rabbitwhile <vallsarnau19@gmail.com>
2015-11-20 10:41:26 +0100

    Update main.cc

--- main.cc
+++ main.cc
@@ -16,7 +16,11 @@ int main() {
  Supermercat super;
  string condicio;
  while(getline(cin,condicio)){
-  if (condicio == "inicialitzar") super.inicialitza();
+  if (condicio == "inicialitzar"){
+   int r,c,x,n;
+   cin >> r >> c >> x >> n;
+   super.inicialitza(r,c,x,n);
+  } 
   else if (condicio == "carregar") super.carrega();
   else if (condicio == "simular") super.simula();
   else if (condicio == "informacio") super.informa();
