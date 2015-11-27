f026ac75f7e3084862fc5972349164dafc5aba59
rabbitwhile <vallsarnau19@gmail.com>
2015-11-27 12:01:02 +0100

    Update Rellotge.hh

--- Rellotge.hh
+++ Rellotge.hh
@@ -17,9 +17,7 @@ using namespace std;
 class Rellotge{
 
 private:	
-	string hora; /* Hora en format HH:MM:SS. */
-
-
+	string hora; // Hora en format HH:MM:SS. 
 public:
 	
  	/* Constructores */
@@ -44,13 +42,6 @@ public:
 		\post Retorna l’hora del paràmetre implícit en format string (HH:MM).
 	*/
 	string consulta_hora();
-
-
-	/** @brief Compara dues hores.
-		\pre <em>hora1, hora2</em> són dues hores en format string.
-		\post Retorna 0 si són iguals, 1 si <em>hora1</em> és anterior a <em>hora2</em>, 2 altrament.
-	*/
-	int compara_hores(string hora1, string hora2);
 	
 	
 	/** @brief Compara dos rellotges.
@@ -58,23 +49,15 @@ public:
 		\post Retorna 0 si són iguals, 1 si el paràmetre implícit és anterior a 
 		<em>R2</em>, 2 altrament.
 	*/
-	//int compara_rellotges(Rellotge R2);
+	int compara_rellotges(Rellotge R2);
 
 
 	/* Modificadores */
-	/** @brief Modifica l’hora del rellotge.
-		\pre Cert
-		\post Modifica l’hora del paràmetre implícit segons parametre explicit.
-	*/
-	//void modifica_hora(string h);
 
-	/** @brief Modifica la data del rellotge.
-		\pre Cert
-		\post Modifica la data del paràmetre implícit segons parametre explicit.
-	*/
-	//void modifica_data(string d);
 	
 	void suma_temps(int s); //suma segons al parametre implicit
+	
+	void llegir_hora();
 };
 
 #endif
