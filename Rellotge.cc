929bc8604bb23396dfaf9afef39db91226853cee
rabbitwhile <vallsarnau19@gmail.com>
2015-11-27 11:48:19 +0100

    Update Rellotge.cc

--- Rellotge.cc
+++ Rellotge.cc
@@ -9,12 +9,14 @@ Rellotge::Rellotge() {
 
 Rellotge::~Rellotge() {}
 
-string Rellotge::consulta_temps() {
-	return temps_cobrament;
+string Rellotge::consulta_hora() {
+	return hora;
 }
 
 
-int Rellotge::compara_hores(string hora1, string hora2) {
+int Rellotge::compara_rellotges(Rellotge R2) {
+	hora1 = hora;
+	hora2 = R2.consulta_hora();
 	if (hora1==hora2) return 0;
 	else {
 		int h1, h2;
@@ -27,13 +29,25 @@ int Rellotge::compara_hores(string hora1, string hora2) {
 			m1=(hora1[3]*10)+hora1[4];
 			m2=(hora2[3]*10)+hora2[4];
 			if (m1>m2) return 2;
-			else return 1;
+			if (m2>m1)return 1;
+			else {
+				int s1,s2;
+				s1=(hora1[6]*10)+hora1[7];
+				s2=(hora2[6]*10)+hora2[7];
+				if (s1>s2) return 2;
+				else return 1;
+			}
 			
 		}
+		
 	}
 }
 
-int Rellotge::compara_rellotges(Rellotge R2) {
+void suma_temps(int s){
+	
+}
+
+/*int Rellotge::compara_rellotges(Rellotge R2) {
 	int h, d;
 	h = compara_hores(hora, R2.consulta_hora());
 	//d = compara_dates(data, R2.consulta_data());
@@ -47,7 +61,7 @@ int Rellotge::compara_rellotges(Rellotge R2) {
 		else return 2;
 	}
 }
-
+*/
 
 /*void Rellotge::modifica_hora(string h) {
 	hora=h;
@@ -60,6 +74,4 @@ void Rellotge::modifica_data(string d) {
 }
 */
 
-void suma_temps(int s){
-	
-}
+
