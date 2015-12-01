a92366374f8dd45cebcd255985334a29c9ffe327
rabbitwhile <vallsarnau19@gmail.com>
2015-12-01 15:52:45 +0100

    Update Client.cc

--- Client.cc
+++ Client.cc
@@ -4,37 +4,30 @@
 Client::Client() {
 }
 
-  
-  
-  //Client::Client(int torn, int caixa, int producte, Rellotge R); //el rellotge és el temps de recollida del tiquet
-  
-  
 Client::~Client() {
 }
 
-
-
-   void Client::afegir_caixa(int c) {
+void Client::afegir_caixa(int c) {
     caixa = c;
    }
    
-   int Client::consultar_torn() const {
+int Client::consultar_torn() const {
     return torn;
    }
    
-   int Client::consultar_caixa() const {
+int Client::consultar_caixa() const {
     return caixa;
    }
    
-   int Client::consultar_num_productes() const {
+int Client::consultar_num_productes() const {
      return num_productes;
    }
    
-   string Client::consultar_hora() {
+string Client::consultar_hora() {
     return R.consulta_hora();
    }
    
-   void Client::llegeix_Client() {
+void Client::llegeix_Client() {
      cin >>  torn;
      R.llegir_hora();
      cin >> num_productes;
